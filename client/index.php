da5e07353ef8b117df4e3bde1e25a3b16041c386
Nikolay Zinov <nzinov@gmail.com>
2014-11-09 09:01:33 +0300

    modify index page design

--- client/index.php
+++ client/index.php
@@ -1,5 +1,4 @@
 <?
-error_reporting(E_ALL);
 session_start();
 $state = $_SESSION['state'] = md5(rand());
 $signed_in = isset($_SESSION['id']);
@@ -65,12 +64,9 @@ if ($page == "map")
     <div class="navbar navbar-default">
           <a class="navbar-brand" href="index.php">Морской бой по-физтеховски</a>
             <ul class="nav navbar-nav">
-              <li id="map"><a class="indevelop" rel="tooltip" title="Временно недоступно" data_href="index.php?page=map">Карта</a></li>
-              <li id="about"><a href="index.php?page=about">Об игре</a></li>
               <li id="rules"><a href="index.php?page=rules">Правила</a></li>
 			  <li id="rate"><a href="index.php?page=rate">Рейтинг</a></li>
 			  <li id="bugreport"><a target="blank" href="https://github.com/nzinov/fizteh-seabattle/issues?state=open">Сообщить об ошибке</a></li>
-			  <li id="ai"><a class="indevelop" href="#" rel="tooltip" title="Разрабатывается">Бой программ</a></li>
             </ul>
 <?
 if ($signed_in)
@@ -79,17 +75,17 @@ if ($signed_in)
      $res = mysql_query("SELECT `id` FROM `games` WHERE (`first`='$id' or `second`='$id') AND `type` IN (2, 3) LIMIT 1;");
 	 if (mysql_num_rows($res) > 0)
 	 {
-		echo "<a href=\"game.php/".mysql_result($res,0,'id')."\" class=\"btn btn-success navbar-btn\">В игру</a>";
+		echo "<a href=\"game.php/".mysql_result($res,0,'id')."\" class=\"btn btn-success navbar-btn navbar-right\">В игру</a>";
 		$alert_type = 'warning';
 		$alert = "У вас есть активная игра: нажмите на кнопку в верхнем меню, чтобы начать её.";
 	 }
 	 mysql_query("UPDATE `users` SET `online`=NOW() WHERE `id` = '$id';");
-     echo "<img class='pull-left img-circle' height=45 src='{$_SESSION['image_url']}' /><p class=navbar-text>{$_SESSION['name']}</p>";
+     echo "<div class='navbar-right'><img class='img-circle pull-left' height=45 src='{$_SESSION['image_url']}' /><p class=navbar-text>{$_SESSION['name']}</p></div>";
 }
 else
 {
 ?>
-    <button id='signinButton' class='btn btn-primary navbar-btn'>Войти через Google</button>
+    <button id='signinButton' class='btn btn-primary navbar-btn navbar-right'>Войти через Google</button>
     <script type="text/javascript" src="https://apis.google.com/js/client:plusone.js"></script>
     <script type="text/javascript">
        function onload()
@@ -124,7 +120,7 @@ for ($i = 0; $i < $count; $i++)
 $content .= "<li>".mysql_result($res,$i,'name')."</li>";
 }
 $content .= "</ul>";
-echo "<button id=\"online\" class=\"btn btn-info navbar-btn pull-left\">$count онлайн</button>";
+echo "<button id=\"online\" class=\"btn btn-info navbar-btn navbar-left\">$count онлайн</button>";
 echo "<script> $('#online').popover({placement: 'bottom', trigger: 'hover', title: 'Пользователи онлайн', content: '$content', html: true});</script>";
 ?>
       </div>
@@ -146,29 +142,20 @@ if ($page == "main")
 	<div class="jumbotron">
         <h1>Добро пожаловать</h1>
         <p><? echo $rand;?> - Морской бой по-физтеховски!</p>
-        <p><a class="btn btn-primary btn-large" href="index.php?page=about">Узнать больше &raquo;</a></p>
+        <p><a class="btn btn-primary btn-large" href="index.php?page=rules">Прочесть правила&raquo;</a></p>
      </div>
 <div class="row">
-<div class="col-md-12">
+<div class="col-md-12 panel panel-default">
+<div class="panel-body">
+<p>Морской бой по-физтеховски - это настольная стратегическая игра для двух игроков, похожая на шашки или шахматы.
+Игроки по-очереди передвигают свои фишки по клетчатому полю. Основным отличием является то, что игрок видит местоположение фишек соперника, но не их тип. А расставляет он фишки перед началом игры по своему усмотрению. Поэтому остается только догадываться, каким кораблем противник идет в атаку.</p>
+<p>Морской бой появился в 80-х годах прошлого века на физфаке МГУ. Бытовало мнение, что придумали его в МФТИ - отсюда и название, - но подтверждений этому нет.
+Он основывается на игре "Stratego" ("Сражение", "Разведка боем" в советском варианте), но гораздо сложнее и интересней.</p>
+<p>На этом сайте представлена сетевая версия Морского боя по-физтеховски</p>
+</div>
+</div>
+</div>
 <?php
-$res = mysql_query("SELECT * FROM `news` ORDER BY `added` DESC LIMIT 10");
-while ($news = mysql_fetch_array($res))
-{
-echo "
-<div class='panel panel-{$colors[$news['color']]}'>
-  <div class='panel-heading'>{$news['title']}</div>
-  <div class='panel-body'>{$news['content']}</div>
-  <div class='panel-footer'>{$news['added']}</div>
-</div>";
-}
-if ($admin)
-{
-echo form(array("action" => "addnews", "reverse" => "index.php"),
-input_text("title","Заголовок"),
-input_textarea("content","Текст новости"),
-input_select("color", "Цвет", $colors));
-}
-echo "</div></div>";
 }
 else if ($page == "map")
 {
@@ -264,97 +251,16 @@ else if ($page == "map")
 	  </div>
 	  <?php
 	  }
-	  else if ($page == "about")
-	  {
-	  ?>
-	  <ul id="subnav" class="nav nav-pills">
-      <li class="active"><a href="#a-history">История игры</a></li>
-      <li><a href="#a-descr">Описание</a></li>
-      <li><a href="#a-rate">Рейтинг</a></li>
-	  <li><a href="#a-tech">Технические подробности</a></li>
-      </ul>
-	  <div class="row">
-	  <div class="col-md-12">
-	  <div id="a-history">
-	  <h2>История игры</h2>
-	  <p>
-	  Эта игра, скорее всего, появилась в конце 80-х годов прошлого столетия, но не в МФТИ, как можно было бы подумать по названию, а на физфаке МГУ. 
-	  Название объясняется тривиально: на этом самом факультете бытует легенда, что Морской бой пришел с физтеха.)
-	  </p>
-	  <p>
-	  Потом эта игра распространилась по другим факультетам и институтам. Четкого набора правил нет: наа каждом из них формировался свой тип правил. 
-	  На физфаке они тоже со временем менялись, в основном добавлялись новые фишки.
-	  </p>
-	  <p>
-	  К началу 90-х на многих факультетах существовали рейтинги, проводились турниры. Дальше следы теряются...
-	  </p>
-	  <p>
-	  ***
-	  </p>
-	  <p>Когда я расказал правила своим родственникам, оказалось, что дядя играл в детстве в похожую игру, называвшуюся "Разведка боем". 
-	  В ней вместо кораблей были солдаты и не было блоков и специальных фишек, вроде Торпед.</p>
-	  <p>
-	  Несмотря на такую известность, на широких просторах интернета удалось найти только одно внятное упоминание Морского боя по-физтеховски: 
-	  Статью Ю.Шихова на сайте http://www.fieldofbattle.ru/.
-	  Кроме приведенной выше истории игры и правил, в ней было написано, что студенты делали многочисленные безуспешные попытки 
-	  воплотить эту игру на компьютере. Прочтя это я решил, что вот отличный вариант сделать то, чего еще никто до тебя не сделал, и принялся за работу.
-	  <p/>
-	  <p>
-	  То, что вы видите на своем экране, уже четвертый и, я уверен, последний вариант программы. 
-	  Первые были абсолютно нежизнеспособными и полностью выкидывались. 
-	  Немного о том, как сделан этот морской бой можно прочести в разделе Технические подробности.
-	  </p>
-	  </div>
-	  <div id="a-descr">
-	  <!--<h2>Описание</h2>
-	  <p>
-	  
-	  </p>-->
-	  </div>
-	  <div id="a-feild">
-	  </div>
-	  <div id="a-rate">
-	  <h2>Рейтинг</h2>
-	  Используется модифицированный рейтинг Эло.
-	  Начальный рейтинг равен 22000.
-	  Новый рейтинг высчитывается по формуле R' = R + (S - E), где R - старый рейтинг, S - фактически набранные очки (100 - победа, -100 - поражение), E - ожидаемые очки, взятые из стандартной таблицы.
-	  </div>
-	  <div id="a-tech">
-	  <h2>Технические подробности</h2>
-	  <p>
-	  Как и любое похожее приложение, Морской бой по-физтеховски состоит из клиентской и серверной части. 
-	  Клиентская часть запускается в вашем браузере, а серверная на платформе облачного хостинга <a href="openshift.com">openshift.com</a>.
-	  </p>
-	  <p>
-	  Сервер разделен на два модуля (которые даже работают на разных машинах): часть, отвечающая за отображение сайта 
-	  (использующая обычную связку apache + php + mysql) и часть, написанная на C#, обрабатывающая логику самой игры, 
-	  которая содержит самописный сервер протокола WebSockets.
-	  </p>
-	  <p>
-	  Клиентская часть написана на html + javascript. Для отображения сайта используется framework <a href="http://getbootstrap.com">Twitter Bootstrap</a>
-	  </p>
-	  <p>
-	  Взаимодействия клиента и сервера во время игры происходит посредством обмена объектами JSON по протоколу WebSocket.
-	  </p>
-	  <p>
-	  Для обработки JSON на сервере используется <a href="http://json.codeplex.com/">Json.NET</a>, а на клиенте - <a href="http://www.json.org/js.html">json.js</a>;
-	  Наполнение DIY катриджа openshift, поддерживающего .NET часть сервера 
-	  частично заимствовано <a href="https://github.com/wshearn/openshift-community-cartridge-mono">отсюда</a>
-	  </p>
-	  </div>
-	  </div>
-	  </div>
-	  <?php
-	  }
 	  else if ($page == "rules")
 	  {
 		include("rules.php");
 	  }
 	  else if ($page == "rate")
 	  {
+echo "<h3 class=text-center'>Рейтинг Эло всех зарегистрированных игроков</h3>";
 $sql = "SELECT `name`,`rate` FROM `users` ORDER BY `rate` DESC";
 $res=mysql_query($sql);
-echo "<div class=\"row\"><div class=\"span12\"><table class=\"table table-striped\"><thead><tr><th>#</th><th>Имя</th><th>Рейтинг</th></tr></thead><tbody>";
+echo "<div class=\"row\"><div class=\"col-md-12\"><table class=\"table table-striped\"><thead><tr><th>#</th><th>Имя</th><th>Рейтинг</th></tr></thead><tbody>";
 $last = 0;
 for ($n=0;$n<mysql_num_rows($res);$n++) {
 	$name=mysql_result($res,$n,'name');
